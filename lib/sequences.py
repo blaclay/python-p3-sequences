ca611d898f9c5b570974c3d6df91bfc370200cd5
blaclay <clayblackwelder@gmail.com>
2023-09-21 23:13:14 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+
+    n1, n2 = 0, 1
+    count = 0
+
+    if length > 0:
+        while count < length:
+            my_list.append(n1)
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+
+    print(my_list)
